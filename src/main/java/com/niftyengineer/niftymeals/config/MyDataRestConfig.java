29dc2ae082d98ff113dab612eb761599ef564dc0
nifty-engineer <abraham.olude.ao@gmail.com>
2024-05-07 17:36:27 -0500

    Establish secure communication on the app

---
+++ src/main/java/com/niftyengineer/niftymeals/config/MyDataRestConfig.java
@@ -0,0 +1,41 @@
+package com.niftyengineer.niftymeals.config;
+
+import com.niftyengineer.niftymeals.entity.Meal;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {
+          HttpMethod.POST,
+          HttpMethod.PATCH,
+          HttpMethod.DELETE,
+          HttpMethod.PUT
+        };
+
+        config.exposeIdsFor(Meal.class);
+
+        disableHttpMethods(Meal.class, config, theUnsupportedActions);
+
+        /* Configure CORS Mapping */
+        cors.addMapping(config.getBasePath() + "/**")
+          .allowedOrigins(theAllowedOrigins);
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+          .forDomainType(theClass)
+          .withItemExposure((metadata, httpMethods) ->
+            httpMethods.disable(theUnsupportedActions))
+          .withCollectionExposure((metadata, httpMethods) ->
+            httpMethods.disable(theUnsupportedActions));
+    }
+}
