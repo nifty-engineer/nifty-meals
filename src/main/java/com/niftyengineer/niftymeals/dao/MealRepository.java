a3095229f4d50f64a934689951b563d650d8cad0
nifty-engineer <abraham.olude.ao@gmail.com>
2024-05-12 16:13:27 -0500

    Set up the repository to find meals based on words found in the title

--- src/main/java/com/niftyengineer/niftymeals/dao/MealRepository.java
+++ src/main/java/com/niftyengineer/niftymeals/dao/MealRepository.java
@@ -1,8 +1,13 @@
 package com.niftyengineer.niftymeals.dao;
 
 import com.niftyengineer.niftymeals.entity.Meal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface MealRepository extends JpaRepository<Meal, Long> {
+
+    Page<Meal> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 
 }
